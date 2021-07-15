54e1eb6a75d382c0c520f4df65283c4e32a98364
Vladimir Stepanchikov <volodyavolga@protonmail.ch>
2021-07-15 22:29:24 +0300

    update FabricRequest

--- TestAppD2/Classes/Models/FabricRequest.swift
+++ TestAppD2/Classes/Models/FabricRequest.swift
@@ -8,47 +8,47 @@
 
 import UIKit
 
-class FabricRequest: NSObject {
+class FabricRequest {
+
+    private static let protocolHostPath = "https://api.stackexchange.com/2.2/questions"
+    private static let parameter = "order=desc&sort=activity&site=stackoverflow&key=G*0DJzE8SfBrKn4tMej85Q(("
     
     class func request(tagged stringTagged: String?,
                        numberOfPageToLoad: Int,
                        withBlock completionHandler: @escaping (_ data: Data?) -> Void) {
-        let protocolHostPath = "https://api.stackexchange.com/2.2/questions"
-        let parametrs = "order=desc&sort=activity&site=stackoverflow&key=G*0DJzE8SfBrKn4tMej85Q(("
-        let stringURL = protocolHostPath + "?" + parametrs + "&pagesize=50&tagged=" + stringTagged! + String(format: "&page=%ld", numberOfPageToLoad)
+
+        guard let stringTag = stringTagged else { return }
+        let stringURL = protocolHostPath + "?" + parameter + "&pagesize=50&tagged=" + stringTag + String(format: "&page=%ld", numberOfPageToLoad)
 
         if CacheWithTimeInterval.objectForKey(stringURL) == nil {
-            let stringURL = protocolHostPath + "?" + parametrs + "&pagesize=50&tagged=" + stringTagged! + String(format: "&page=%ld", numberOfPageToLoad)
-            var request = URLRequest(url: URL(string: stringURL)!)
-            request.httpMethod = "GET"
+            guard let url = URL(string: stringURL) else { return }
 
-            let defaultConfiguration = URLSessionConfiguration.default
-            let defaultSession = URLSession(configuration: defaultConfiguration)
-
-            let task: URLSessionDataTask = defaultSession.dataTask(with: request) { (data, response, error) in
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error { print(error); return }
+                if let response = response { print(response) }
+                guard let data = data else { return }
                 completionHandler(data)
                 CacheWithTimeInterval.set(data: data, for: stringURL)
-            }
-            task.resume()
+            }.resume()
         } else {
             completionHandler(CacheWithTimeInterval.objectForKey(stringURL))
         }
     }
     
-    class func request(withQuestionID questionID: Int, withBlock completionHandler: @escaping (_ data: Data?) -> Void) {
-        let protocolHostPath = "https://api.stackexchange.com/2.2/questions"
-        let parametrs = "order=desc&sort=activity&site=stackoverflow&key=G*0DJzE8SfBrKn4tMej85Q(("
+    class func request(withQuestionID questionID: Int,
+                       withBlock completionHandler: @escaping (_ data: Data?) -> Void) {
+        let stringURL = String(format: "%@/%li/answers?%@&filter=!9YdnSMKKT",
+                               protocolHostPath,
+                               questionID,
+                               parameter)
 
-        let stringURL = String(format: "%@/%li/answers?%@&filter=!9YdnSMKKT", protocolHostPath, questionID, parametrs)
-        var request = URLRequest(url: URL(string: stringURL)!)
-        request.httpMethod = "GET"
+        guard let url = URL(string: stringURL) else { return }
 
-        let defaultConfiguration = URLSessionConfiguration.default
-        let defaultSession = URLSession(configuration: defaultConfiguration, delegate: nil, delegateQueue: OperationQueue.main)
-
-        let task: URLSessionDataTask = defaultSession.dataTask(with: request) { (data, response, error) in
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error { print(error); return }
+            if let response = response { print(response) }
+            guard let data = data else { return }
             completionHandler(data)
-        }
-        task.resume()
+        }.resume()
     }
 }
